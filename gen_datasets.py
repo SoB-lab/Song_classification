4b55eed3ab9509568768cbeb9ad254263ba730cc
SoB-lab <81715768+SoB-lab@users.noreply.github.com>
2021-04-12 16:20:26 -0500

    Update

--- gen_datasets.py
+++ gen_datasets.py
@@ -4,32 +4,65 @@ The dataset are now only picture based
 '''
 
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
 from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import matplotlib.pyplot as plt
 
 
+print(tf.__version__)
+print(keras.__version__)
 
-data_dir = 'picture_database/'
+def CNN_model(model, num_classes):
+    model.add(layers.Conv2D(8, 3, padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(16, 3, padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(32, 3, padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Conv2D(64, 3, padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D())
+    model.add(layers.Flatten())
+    # layers.Dense(1024, activation='relu'),
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(num_classes))
 
-batch_size=32
+    return model
+
+data_dir_train = 'picture_database_separated/train/'
+data_dir_test = 'picture_database_separated/test/'
+
+batch_size = 8
+img_height = 400
+img_width = 1200
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
+    data_dir_train,
     validation_split=0.2,
     subset="training",
     seed=123,
+    image_size=(img_height, img_width),
     batch_size=batch_size
 )
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
+    data_dir_train,
     validation_split=0.2,
     subset="validation",
     seed=123,
+    image_size=(img_height, img_width),
     batch_size=batch_size
 )
 
-print(type(train_ds), type(val_ds))
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir_test,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+print(type(train_ds), type(val_ds), type(test_ds))
 
 class_names = train_ds.class_names
 print(class_names)
@@ -40,14 +73,62 @@ for image_batch, labels_batch in train_ds:
     print(labels_batch.shape)
     break
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+#AUTOTUNE = tf.data.experimental.AUTOTUNE
+#train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+#val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3))
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image))
+
+
+# Beginning of the model
+
+model = Sequential()
+
+model = CNN_model(model, num_classes)
+
+tf.keras.optimizers.Adam(
+    learning_rate=0.001,name='Adam'
+)
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+epochs = 15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs)
+
+model.summary()
+
+model.save('my_model')
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
